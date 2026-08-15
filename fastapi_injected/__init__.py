e4faec1c11f9a1bb509bb6843917ed790a0c8102
Yurii Karabas <1998uriyyo@gmail.com>
2026-08-15 13:28:46 +0200

    Add missed import

--- fastapi_injected/__init__.py
+++ fastapi_injected/__init__.py
@@ -1,5 +1,5 @@
 from ._deps_tp import is_dep, unwrap_dep_dependency, unwrap_dep_tp
-from ._fastapi_lifecycle import init_inject_scope
+from ._fastapi_lifecycle import add_injected_scope, init_inject_scope
 from .inject import inject
 from .overrides import FactoryOverride, ValueOverride, push_overrides
 from .resolve import resolve
@@ -12,6 +12,7 @@ __all__ = [
     "FactoryOverride",
     "Injected",
     "ValueOverride",
+    "add_injected_scope",
     "init_inject_scope",
     "inject",
     "is_dep",
